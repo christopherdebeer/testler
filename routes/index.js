86fa27dd4b37487644c6e7eeb78dda0b5ccffc74
Christopher de Beer <christopherdebeer@gmail.com>
2012-01-13 10:49:41 +0000

    output json test

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,22 @@
  * GET home page.
  */
 
-module.exports = function(app){
+module.exports = function (app) {
 
-	app.get("/", function(req,res){
+	app.get("/", function (req,res) {
 		res.render('index', { title: 'Testler' })
+	})
+
+
+	app.get("/testler.json", function (req,res) {
+		res.json({
+			"test-123456": {
+				content: "This is the right copy"
+			},
+			"test-123457": {
+				content: "This is test2 copy"
+			}
+		});
 	})
   
 };
