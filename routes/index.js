a898a7f11ae104f5cac8f836784495713dcc3656
Christopher de Beer <christopherdebeer@gmail.com>
2012-01-13 10:54:53 +0000

    added get & post to testler.json serving [test]

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,28 @@
  * GET home page.
  */
 
-module.exports = function (app) {
-
-	app.get("/", function (req,res) {
-		res.render('index', { title: 'Testler' })
-	})
-
-
-	app.get("/testler.json", function (req,res) {
-		res.json({
+ var testObj = {
 			"test-123456": {
 				content: "This is the right copy"
 			},
 			"test-123457": {
-				content: "This is test2 copy"
+				class: "variantB"
 			}
 		});
-	})
+	};
+
+module.exports = function (app) {
+
+	app.get("/", function (req,res) {
+		res.render('index', { title: 'Testler' })
+	});
+
+	app.get("/testler.json", function (req,res) {
+		res.json(testObj);
+	)};
+
+	app.post("/testler.json", function (req,res) {
+		res.json(testObj)
+	});
   
 };
